84696cdf417ea39d9032644820373c1cce1a8324
Felorez <kingtheiogamehop1@gmail.com>
2025-02-23 00:20:39 +0500

    Добавил таблицу, и собственно сделал, то что сказали

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@
       <label>Ширина: <input type="number" v-model="canvasSize.width" @change="updateCanvasSize" /></label>
       <label>Высота: <input type="number" v-model="canvasSize.height" @change="updateCanvasSize" /></label>
       <button @click="addText">Добавить текст</button>
+      <button @click="addTable">Добавить таблицу</button>
       <button @click="generateHTML">Сохранить HTML</button>
       <!-- Вывод свойств выбранного элемента -->
       <div v-if="selectedElement" class="properties-panel">
@@ -22,11 +23,12 @@
         wheel-unlock-key="Control"
         style="width: 100%; height: 100%;"
       >
-        <template #default="{ disableInteraction }">
+        <template #overlay>
           <VueZoomable 
           id="vue-zoomable"
           selector="#canvas"
           style="width: 100%; height: 100%;"
+          :class="{ 'add-text-cursor': addMode }"
           :minZoom="0.3"
           :maxZoom="2"
           :dblClickZoomStep="0.4"
@@ -36,21 +38,46 @@
           v-model:pan="pan"
           v-model:zoom="zoom">
             <div id="canvas">
-              <div class="canva" :style="{ 
+              <div 
+              class="canva"
+              :style="{ 
                 width: canvasSize.width + 'px', 
                 height: canvasSize.height + 'px'
-                }">
-                <div
-                  v-for="el in elements"
-                  :key="el.id"
-                  class="text resizable"
-                  :data-id="el.id"
-                  :style="{ left: el.x + 'px', top: el.y + 'px', width: el.width + 'px', height: el.height + 'px' }"
-                  contenteditable="true"
-                  @input="(event) => el.text = event.target.innerText"
-                  @click="selectElement(el)"
+              }"
+              @click="onCanvasClick">
+                <div 
+                v-for="el in elements"
+                :key="el.id"
+                class="container-element"
                 >
-                Текст
+                  <div
+                    v-if="'text' in el"
+                    class="text resizable"
+                    :data-id="el.id"
+                    :style="textStyle(el)"
+                    contenteditable="true"
+                    @input="(event) => el.text = event.target.innerText"
+                    @click="selectElement(el)"
+                  >
+                  Текст
+                  </div>
+                  <table
+                   v-if="'rows' in el"
+                   contenteditable="true" 
+                   class="table-element resizable"
+                   :data-id="el.id"
+                   :style="tableStyle(el)"
+                   @input="(e) => onTableInput(e, el)">
+                    <tr v-for="(row, rowIndex) in el.rows" :key="rowIndex">
+                      <td
+                        v-for="(cell, cellIndex) in row"
+                        :key="cellIndex"
+                        contenteditable="true"
+                      >
+                        {{ cell }}
+                      </td>
+                    </tr>
+                  </table>
                 </div>
               </div>
             </div>
@@ -83,15 +110,79 @@ const elements = ref([]);
 // Выбранный элемент
 const selectedElement = ref(null);
 
+const addMode = ref(false);
+
+const whatsMode = ref({});
+
 const addText = () => {
-  elements.value.push({
+  addMode.value = true;
+  
+  whatsMode.value = {
+      id: Date.now(),
+      text: "Текст",
+      x: 0,
+      y: 0,
+      width: null,
+      height: null,
+      offset: { x: 0, y: 0 }
+  };
+};
+
+const addTable = () => {
+  addMode.value = true;
+
+  const defaultRows = [
+    ["Ячейка 1-1", "Ячейка 1-2", "Ячейка 1-3"],
+    ["Ячейка 2-1", "Ячейка 2-2", "Ячейка 2-3"],
+    ["Ячейка 3-1", "Ячейка 3-2", "Ячейка 3-3"]
+  ];
+
+  whatsMode.value = {
     id: Date.now(),
-    text: "Текст",
-    x: 50,
-    y: 50,
-    width: 150,
-    height: 50,
-  });
+    type: "table",
+    rows: defaultRows,
+    x: 0,
+    y: 0,
+    width: null,
+    height: null,
+    offset: { x: 0, y: 0 }
+  };
+};
+
+const textStyle = (el) => {
+  return {
+    position: 'absolute',
+    left: el.x + 'px',
+    top: el.y + 'px',
+    width: el.width !== null ? el.width + 'px' : 'auto',
+    height: el.height !== null ? el.height + 'px' : 'auto',
+    transform: `translate(${el.offset.x}px, ${el.offset.y}px)`
+  }
+};
+
+const tableStyle = (el) => {
+  return {
+    position: 'absolute',
+    left: el.x + 'px',
+    top: el.y + 'px',
+    width: el.width !== null ? el.width + 'px' : 'auto',
+    height: el.height !== null ? el.height + 'px' : 'auto',
+    transform: `translate(${el.offset.x}px, ${el.offset.y}px)`
+  }
+};
+
+const onCanvasClick = (e) => {
+  if (!addMode.value) return;
+
+  const canva = e.currentTarget;
+  const rect = canva.getBoundingClientRect();
+
+  whatsMode.value.x = (e.clientX - rect.left) / zoom.value;
+  whatsMode.value.y = (e.clientY - rect.top) / zoom.value;
+
+  elements.value.push({ ...whatsMode.value });
+
+  addMode.value = false;
 };
 
 const selectElement = (el) => {
@@ -150,7 +241,6 @@ onMounted(() => {
           const el = elements.value.find(e => e.id == id);
           if (!el) return;
 
-          // Обновляем координаты элемента
           el.x += event.dx / zoom.value;
           el.y += event.dy / zoom.value;
 
@@ -177,12 +267,15 @@ onMounted(() => {
         el.width = event.rect.width / zoom.value;
         el.height = event.rect.height / zoom.value;
 
+        el.offset.x +=  event.deltaRect.left
+        el.offset.y += event.deltaRect.top
+
         setTimeout(() => {
             isPanEnabled.value = true;
         }, 750);
       }
     },
-    inertia: true
+    invert: 'reposition'
   });
 });
 </script>
@@ -244,5 +337,22 @@ onMounted(() => {
   background: #3c3c3c;
   border: 1px solid #555;
   font-size: 14px;
+}
+
+.add-text-cursor {
+  cursor: crosshair; /* Или другой желаемый стиль курсора */
+}
+
+.table-element {
+  width: 100%;
+  height: 100%;
+  border-collapse: collapse;
+}
+.table-element td {
+  border: 1px solid #ccc;
+  padding: 5px;
+  min-width: 50px;
+  min-height: 30px;
+  white-space: pre-wrap;
 }
 </style>
